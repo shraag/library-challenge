91b4ba80da748ff59c7b0702b53cd08a65ce1c29
Shranay Agarwal <shranay.agarwal@gmail.com>
2024-10-09 13:14:07 +0530

    updated appp init with more initialisations

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,36 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
+
+# Initialize extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)  # Initialize JWTManager
+
+# Load configuration from config.py
+app.config.from_object(Config)
+
+
+# Default route
+@app.route('/')
+def index():
+    return jsonify({"message": "Welcome to the Library Management System"}), 200
+
+# Error handler for 404 - Not Found
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+# Error handler for 500 - Internal Server Error
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"error": "Internal server error"}), 500
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
