bd9ed4d3aacf48806e56fe3750a6257718d886a7
Shranay Agarwal <shranay.agarwal@gmail.com>
2024-10-10 01:53:46 +0530

    added repo functions to suppot librarian routes

--- modules/library/repository/repository.py
+++ modules/library/repository/repository.py
@@ -1,5 +1,5 @@
 from modules.library.repository.models import User, Book, BorrowedBook, MemberHistory
-from modules.library.models.requests import SignUpRequest, BookRequest
+from modules.library.models.requests import SignUpRequest, BookRequest, UpdateMemberRequest
 from app import db
 from typing import List, Dict
 
@@ -30,3 +30,36 @@ class LibraryRepository:
     def get_books(self) -> List[Dict]:
         books = Book.query.all()
         return [book.to_dict() for book in books]
+    
+    def delete_book(self, book_id: str) -> None:
+        Book.query.filter_by(id=book_id).delete()
+        db.session.commit()
+
+    def update_book(self, book_id: str, book_request: BookRequest) -> None:
+        Book.query.filter_by(id=book_id).update({
+            'title': book_request.title,
+            'author': book_request.author,
+            'published_year': book_request.published_year
+        })
+        db.session.commit()
+
+    def delete_user(self, user_id: str) -> None:
+        #soft delete
+        User.query.filter_by(id=user_id).update({'status': 'DELETED'})
+
+    def update_user(self, user_id: str, user_request: UpdateMemberRequest) -> None:
+        if user_request.first_name:
+            User.query.filter_by(id=user_id).update({'first_name': user_request.first_name})
+        if user_request.last_name:
+            User.query.filter_by(id=user_id).update({'last_name': user_request.last_name})
+        if user_request.username:
+            User.query.filter_by(id=user_id).update({'username': user_request.username})
+        db.session.commit()
+
+    def get_member_history(self, member_id: str) -> List[Dict]:
+        member_history = MemberHistory.query.filter_by(member_id=member_id).all()
+        return [history.to_dict() for history in member_history]
+    
+    def get_members(self) -> List[Dict]:
+        members = User.query.filter_by(role='MEMBER').all()
+        return [member.to_dict() for member in members]
