ca2414aecfe817aafc0756b835364999437712cb
Shranay Agarwal <shranay.agarwal@gmail.com>
2024-10-09 16:44:21 +0530

    added flask migration support

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key"
     SQLALCHEMY_BINDS = {
-        'library': 'mysql+pymysql://username:password@localhost/library_db'
+        'library': 'mysql+pymysql://root:8july2001@localhost/library_db'
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your_jwt_secret_key"
