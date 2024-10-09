e2312c2ad8a671e5f201363048713811be27b2d0
Shranay Agarwal <shranay.agarwal@gmail.com>
2024-10-10 01:53:08 +0530

    added all librarian services

--- modules/library/service.py
+++ modules/library/service.py
@@ -1,5 +1,5 @@
 from modules.library.repository.repository import LibraryRepository
-from modules.library.models.requests import SignUpRequest, BookRequest
+from modules.library.models.requests import SignUpRequest, BookRequest, UpdateMemberRequest
 from modules.library.repository.models import User, Book, BorrowedBook, MemberHistory
 from typing import List, Dict
 
@@ -25,3 +25,21 @@ class LibraryService:
 
     def get_books(self) -> List[Dict]:
         return library_repository.get_books()
+    
+    def delete_book(self, book_id: str) -> None:
+        library_repository.delete_book(book_id)
+
+    def update_book(self, book_id: str, book_request: BookRequest) -> None:
+        library_repository.update_book(book_id, book_request)
+    
+    def delete_user(self, user_id: str) -> None:
+        library_repository.delete_user(user_id)
+
+    def update_user(self, member_id: str, user_request: UpdateMemberRequest) -> None:
+        return library_repository.update_user(member_id, user_request)
+    
+    def get_member_history(self, member_id: str) -> List[Dict]:
+        return library_repository.get_member_history(member_id)
+    
+    def get_members(self) -> List[Dict]:
+        return library_repository.get_members()
