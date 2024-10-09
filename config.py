6a2abc2f033aabfdca43605bc3f5e190f4f189c5
Shranay Agarwal <shranay.agarwal@gmail.com>
2024-10-09 18:09:56 +0530

    removed multidatabase setup

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key"
-    SQLALCHEMY_BINDS = {
-        'library': 'mysql+pymysql://root:8july2001@localhost/library_db'
-    }
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8july2001@localhost/library_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your_jwt_secret_key"
     # URL_BASE_PATH = os.getenv('URL_BASE_PATH', '')
