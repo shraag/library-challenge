5c409ed455468ccd3a85f1f9d6701a1b6ae42e80
Shranay Agarwal <shranay.agarwal@gmail.com>
2024-10-09 16:29:25 +0530

    Add URL base path configuration

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config:
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your_jwt_secret_key"
+    # URL_BASE_PATH = os.getenv('URL_BASE_PATH', '')
+    URL_BASE_PATH = '/api'
